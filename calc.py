bb6dea0f30b6abd3dadc9d76014c4d2597c4493c
Lucas Santos da Silva <lucassantosdasilva1@gmail.com>
2018-09-04 22:08:20 -0300

    tdd

---
+++ calc.py
@@ -0,0 +1,28 @@
+def func(x):
+	return x + 5;
+
+
+def teste_answer():
+	assert func(10) == 15
+
+def func(b):
+        return b - 5
+
+
+def teste_answer():
+        assert func(100) ==  95
+
+def func(y):
+        return y * 10
+
+
+def teste_answer():
+        assert func(-3) == -30
+
+def func(a):
+        return a -30 
+
+
+def teste_answer():
+        assert func(-120) == -150
+
