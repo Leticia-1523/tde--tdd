b1ca1c16f9725851b84e9b20d595db40309409f9
Leticia-1523 <42879743+Leticia-1523@users.noreply.github.com>
2018-09-11 21:24:48 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,28 +1,6 @@
-def func(x):
-	return x + 5;
-
-# dedyyged
-def teste_answer():
-	assert func(10) == 15
-
-def func(b):
-        return b - 5
-
-
-def teste_answer():
-        assert func(100) ==  95
-
-def func(y):
-        return y * 10
-
-
-def teste_answer():
-        assert func(-3) == -30
-
-def func(a):
-        return a -30 
-
-
-def teste_answer():
-        assert func(-120) == -150
+def soma(a, b):
+	try:
+		return  float(a)+float(b)
+	except:
+return None
 
