45b0a2bb6cfe2b233c6ae5b809de8336c5c8c5b4
Leticia-1523 <42879743+Leticia-1523@users.noreply.github.com>
2018-09-11 21:27:34 -0300

    Create test_calc.py

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+import pytest
+from calc import soma
+
+def test_soma():
+	assert soma(5, 2) == 7
+	assert soma(1,"4") == 5
+  assert soma("x", "z") == None
+  assert soma(1, "a") == None
+	assert soma("1", "2") == 3
+	assert soma(-5, -5) == -10
+	assert soma(-1, 2) == 1
+	assert soma("y", 2) == None
+	
+  
